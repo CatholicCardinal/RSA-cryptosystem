d33f29b94ec5f959db5a081642ee11f4e6dc6ced
CatholicCardinal <79603714+CatholicCardinal@users.noreply.github.com>
2021-09-03 17:21:35 +0300

    -vers 1.0
    -finish

---
+++ RSA/RSA/Crypt.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Numerics;
+
+
+namespace RSA
+{
+	public class CryptRsa
+	{
+		public int p;
+		public int q;
+		public int n;
+		private int f;
+		public int d;
+		public int e;
+
+		public bool PrimeCheking(int num)
+		{
+			for (long i = 2; i <= (Math.Sqrt(num)); i++)
+				if (num % i == 0)
+					return false;
+			return true;
+		}
+
+		private int MyltyplyN(int p, int k)
+        {
+			return p * k;
+        }
+
+		private int FEller(int p, int k)
+        {
+			return (p-1) * (k-1);
+        }
+		
+		private int Euclid(int a, int b)
+        {
+			int r = 1;
+
+			while (r != 0)
+			{
+				r = a - b * ((int)a/b);
+				if (r == 0)
+				{
+					return b;
+				}
+				else
+				{
+					a = b;
+					b = r;
+				}
+			}
+			return b;
+        }
+
+		private int EuclidExtension(int a, int b)
+        {
+			int r = 1;
+			int[,] Z = new int[2, 2] { { 0, 1 }, { 1,  -((int)(a / b))} };
+			int[,] A = new int[2,2] { { 1, 0 }, { 0, 1 } };
+
+			while(r!=0)
+            {
+				r = a - b * ((int)a / b);
+				if (r==0)
+                {
+					if (A[1, 1] < 0)
+					{
+						A[1, 1] += f;
+					}
+					return A[1,1];
+                }
+                else
+                {
+					A = Multiplication(A, Z);
+					a = b;
+					b = r;
+					Z[1, 1] = -(int)(a / b);
+				}
+            }
+
+			if (A[1,1]<0)
+            {
+				A[1, 1] += f;
+            }
+			return A[1, 1];
+        }
+
+		private int[,] Multiplication(int[,] a, int[,] b)
+		{
+			int[,] r = new int[2, 2];
+			for (int i = 0; i < b.GetLength(1); i++)
+			{
+				for (int j = 0; j < b.GetLength(0); j++)
+				{
+					r[i, j] = 0;
+					for (int k = 0; k < b.GetLength(0); k++)
+					{
+						r[i, j] += a[i, k] * b[k, j];
+					}
+				}
+			}
+			return r;
+		}
+
+		public int[] EComputing()
+        {
+			int[] A= new int[1000];
+           
+
+			n=MyltyplyN(p,q);
+			f = FEller(p, q);
+
+            for (int i = 2, j=0; i < f; i++)
+            {
+				if (PrimeCheking(i) == true)
+                {
+					if (Euclid(f, i) == 1)
+					{
+						A[j] = i;
+						j++;
+					}
+                }
+            }
+			return A;
+        }
+
+		public int DComputing()
+        {
+			d=EuclidExtension(f, e);
+			return d;
+		}
+
+		private BigInteger Binpow(BigInteger a, BigInteger n)
+		{
+			if (n == 0)
+				return 1;
+			if (n % 2 == 1)
+				return Binpow(a, n - 1) * a;
+			else
+			{
+				BigInteger b = Binpow(a, n / 2);
+				return BigInteger.Pow(b, 2);
+			}
+		}
+
+		public char[] Crypting(char[] word)
+        {
+			BigInteger[] A= new BigInteger[100];
+
+            for (int i = 0, j = 0; word[i] != '0'; i++,j++)
+            {
+                if (word[i]==' ')
+                {
+					A[j] = (BigInteger)word[i];
+					i++;
+					j++;
+                }
+				A[j] =(BigInteger)word[i] - 95;
+            }
+
+            for (int i = 0; A[i] != 0 ; i++)
+            {
+				A[i] = (Binpow(A[i], (BigInteger)e)) % (BigInteger)n;
+			}
+
+			for (int i = 0; A[i] != 0; i++)
+			{
+				word[i] = (char)(A[i]);
+			}
+			return word;
+        }
+
+		public char[] Decrypting(char[] word)
+        {
+			BigInteger[] A = new BigInteger[100];
+			
+			for (int i = 0, j = 0; word[i] != '0'; i++, j++)
+			{
+				A[j] = (BigInteger)word[i];
+			}
+
+			for (int i = 0; A[i] != 0; i++)
+			{
+				A[i] = (Binpow(A[i], (BigInteger)d)) % (BigInteger)n;
+			}
+
+			for (int i = 0; A[i] != 0; i++)
+			{
+				if (A[i] == 32)
+				{
+					word[i] = (char)(A[i]);
+					i++;
+				}
+				word[i] = (char)(A[i] + 95);
+			}
+			return word;
+		}
+	}
+}
---
+++ RSA/RSA/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace RSA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        CryptRsa cryptrsa= new CryptRsa();
+        public string ChekingNum(string num, string label)
+        {
+            Regex rx = new Regex("[a-z|а-я]");
+
+            if ((num!="") & (rx.IsMatch(num)!=true) & (num != " "))
+            {
+                if (PrimeCheking(int.Parse(num)) == true)
+                {
+                    label = "true";
+                }
+                if (PrimeCheking(int.Parse(num)) == false)
+                {
+                    label = "false";
+                }
+                return label;
+            }
+            else
+            {
+                return (label = "undef");
+            }
+        }
+
+        public bool PrimeCheking(int num)
+        {
+            for (long i = 2; i <= Math.Sqrt(num); i++)
+                if (num % i == 0)
+                    return false;
+            return true;
+        }
+
+        private void TextBox3_TextChanged(object sender, EventArgs e)
+        {
+            label2.Text = ChekingNum(textBox3.Text, label2.Text);
+            ColorSet(label2);
+        }
+
+        private void TextBox4_TextChanged(object sender, EventArgs e)
+        {
+            label4.Text = ChekingNum(textBox4.Text, label4.Text);
+            ColorSet(label4);
+        }
+
+        public void ColorSet(Label label)
+        {
+            if (label.Text == "true")
+            {
+                label.ForeColor = Color.Green;
+            }
+            if (label.Text == "false")
+            {
+                label.ForeColor = Color.Red;
+            }
+            if (label.Text == "undef")
+            {
+                label.ForeColor = Color.White;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] A=new int[1000];
+            cryptrsa.p = int.Parse(textBox3.Text);
+            cryptrsa.q = int.Parse(textBox4.Text);
+
+            A = cryptrsa.EComputing();
+
+            for (int i = 0; A[i]!=0; i++)
+            {
+                comboBox1.Items.Add(A[i]);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string key;
+
+            cryptrsa.e = int.Parse(comboBox1.Text);
+            key ="{"+comboBox1.Text+", "+cryptrsa.n+"}";
+            label8.Text = key;
+            cryptrsa.DComputing();
+            key = "{" + cryptrsa.d + ", " + cryptrsa.n + "}";
+            label10.Text = key;
+
+            string str = textBox1.Text+"0";
+            char[] ar = str.ToArray<char>();
+
+            ar = cryptrsa.Crypting(ar);
+            str = new string(ar);
+            str=str.Remove(str.Length - 1);
+            textBox2.Text = str;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string str = textBox2.Text+"0";
+            char[] ar = str.ToArray<char>();
+
+            ar = cryptrsa.Decrypting(ar);
+            str = new string(ar);
+            str=str.Remove(str.Length - 1);
+            textBox1.Text = str;
+        }
+    }
+}
